20e2e9f8051d9f063fdc2e47b0e9e57d58249798
akash744 <akashashok12341999@gmail.com>
2020-07-18 00:23:11 +1200

    update 1

--- assets/js/toDo.js
+++ assets/js/toDo.js
@@ -1,12 +1,29 @@
-$("li").click(function(){
+
+$(".fa-plus").click(function(){
+    $("input[type = 'text']").fadeToggle();
+})
+
+$("ul").on("click", "li", function(){
 
     $(this).toggleClass("finished");
     
 });
 
-$("span").click(function(event){
+$("ul").on("click", "#trash", function(event){
 
-    $(this).parent().fadeOut();
+    $(this).parent().fadeOut(500,function(){
+        $(this).remove();
+    });
     event.stopPropgration();
     
 });
+
+$("input[type = 'text']").keypress(function(event){
+    if (event.which === 13){
+        var userToDo = $(this).val();
+        $(this).val("");
+        $("ul").append(
+            "<li><span id = 'trash'><i class='fa fa-trash' aria-hidden='true'></i></span>" + userToDo + " <span id = 'send_right'><i class='fa fa-chevron-circle-right' aria-hidden='true'></i></span></li>");
+    }
+})
+
